d5660eb6cae237bdba3cb182dbb6a6f8a4bbaa7d
rahulbollisetty <rahul20bollisetty@gmail.com>
2020-05-28 23:16:03 +0530

    br tag to newline

--- codeforces2.py
+++ codeforces2.py
@@ -8,10 +8,13 @@ for i in lst:
     url = i
     r = requests.get(url)
     html_cont = r.text
-    soup = BeautifulSoup(html_cont, "lxml")
+    soup = BeautifulSoup(html_cont, 'lxml')
     div = soup.find_all('div', class_ = "input")
     for inp in  div:
         #print(inp.getText())
         pre = inp.find('pre')
-        print(pre.getText())
+        # print(pre.get_text())
+        for i in pre.select('br'):
+            i.replace_with('\n')
+        print(pre.text.strip().strip('\n').strip())
 #great now remove whitespaces and store them in a array or so
