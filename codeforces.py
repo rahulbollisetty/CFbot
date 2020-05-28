97cb30246e1237f2ae4dbc5d76086d232c6ae2b7
rahulbollisetty <rahul20bollisetty@gmail.com>
2020-05-28 23:34:19 +0530

    added problem name list and br to newline

--- codeforces.py
+++ codeforces.py
@@ -11,10 +11,10 @@ soup = BeautifulSoup(html_cont, 'html.parser')
 table = soup.find('table', class_= "problems")
 lst = []
 j = url.replace("https://codeforces.com", "") + "/problem/"
-
+problem_name = []
 for i in table.find_all("td", class_ = "id"):
     a = i.find('a')
-    problem_name = a.replace(a['href'], j)
+    problem_name.append(a['href'].replace(j,""))
     b = url + "/problem/" + a['href'].replace(j, "")
     lst.append(b)
     print(problem_name)
