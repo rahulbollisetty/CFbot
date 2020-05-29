f6f4dedea0b3c99da5ab2158be0c62e5ae6ce016
rahulbollisetty <rahul20bollisetty@gmail.com>
2020-05-29 11:45:20 +0530

    directory create function

--- codeforces.py
+++ codeforces.py
@@ -1,23 +1,31 @@
 import requests
-
 import re
-
 from bs4 import BeautifulSoup
-print("paste the url of contest")
-url = str(input())
+
+print("enter the contest id")
+url1 = str(input())
+
+url = "https://codeforces.com/contest/" + url1
 r = requests.get(url)
 html_cont = r.text
+# parsing the url
+
 soup = BeautifulSoup(html_cont, 'html.parser')
 table = soup.find('table', class_= "problems")
 lst = []
+
+# dynamic url
+
 j = url.replace("https://codeforces.com", "") + "/problem/"
 problem_name = []
+
+# fetching url of every problem
+
 for i in table.find_all("td", class_ = "id"):
     a = i.find('a')
+    # fetching problem name
     problem_name.append(a['href'].replace(j,""))
     b = url + "/problem/" + a['href'].replace(j, "")
+    # storing problem links
     lst.append(b)
     print(problem_name)
-#good now store these links in an array
-# for i in lst:
-#     print(i)
--- codeforces2.py
+++ codeforces2.py
@@ -1,20 +1,48 @@
 import requests
-
 import re
-from codeforces import lst
+from codeforces import lst, problem_name,url1
 from bs4 import BeautifulSoup
-#print("enter url of problem")
+import os
+
+# create a directory for the contest id
+
+cwd = os.getcwd()
+path = os.path.join(cwd, url1)
+try: 
+    os.mkdir(path) 
+except OSError as error: 
+    print(error)    
+os.chdir(path)
+
+j = 0 #cont for no.of problems
 for i in lst:
     url = i
     r = requests.get(url)
     html_cont = r.text
+    # parsing every problem page
     soup = BeautifulSoup(html_cont, 'lxml')
     div = soup.find_all('div', class_ = "input")
+    # creating directoy for each problem
+
+    cwd = os.getcwd()
+    path = os.path.join(cwd, problem_name[j])
+    j += 1
+    try: 
+        os.mkdir(path) 
+    except OSError as error: 
+        print(error)
+
+    os.chdir(path)
+    
+    # finding input div
     for inp in  div:
-        #print(inp.getText())
+        
         pre = inp.find('pre')
-        # print(pre.get_text())
+        
         for i in pre.select('br'):
             i.replace_with('\n')
         print(pre.text.strip().strip('\n').strip())
+    print(str(os.getcwd()))
+    os.chdir('..')
+    print(str(os.getcwd()))
 #great now remove whitespaces and store them in a array or so
