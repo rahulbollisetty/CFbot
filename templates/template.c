cf85c00dd42c02a261a7783ef369ed55b7ccd59a
rahulbollisetty <51826858+rahulbollisetty@users.noreply.github.com>
2020-06-01 21:22:04 +0530

    Update template.c

--- templates/template.c
+++ templates/template.c
@@ -3,10 +3,11 @@ int main()
 { 
     FILE *fp;
     // For getting input from input0.txt file 
+    // Change the input0.txt according to the number of files
+    // make sure to comment this out before submitting it to the online judge
     fp = freopen("input0.txt", "r", stdin); 
     
-    // Change the input0.txt according to the number of files
-    //   make sure to comment this out before submitting it to the online judge
+    
     // Your code goes Here
 
     return 0; 
