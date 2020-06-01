555691e7bf5e2f5156e6c766c22f5d2ae3d73bc7
rahulbollisetty <51826858+rahulbollisetty@users.noreply.github.com>
2020-06-01 21:21:04 +0530

    Update template.py

--- templates/template.py
+++ templates/template.py
@@ -1,5 +1,6 @@
 import sys 
 # For getting input from input.txt file 
+# Change the input0.txt according to the number of files
 # make sure to comment this line below before submitting your code to OJ
 sys.stdin = open('input0.txt', 'r')  
 # your code here
