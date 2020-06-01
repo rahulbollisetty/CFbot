a778b081dace74867e52d90b510175e92800e281
rahulbollisetty <51826858+rahulbollisetty@users.noreply.github.com>
2020-06-01 21:21:43 +0530

    Update template.cpp

--- templates/template.cpp
+++ templates/template.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 int main()
 {
-    // For getting input from input0.txt file 
-    // Change the input0.txt according to the number of files
-    //make sure to comment this out before submitting it to the online judge
+    //make sure to comment the lines below before submitting it to the online judge
     #ifndef ONLINE_JUDGE 
-    // For getting input from input.txt file 
+    // Change the input0.txt according to the number of files
+    // For getting input from input0.txt file 
     freopen("input0.txt", "r", stdin); 
     #endif
     // Your code goes here
